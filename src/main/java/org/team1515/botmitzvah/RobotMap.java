f380d1064f8c80abb9f6dc7aaee2193e5442ed20
Hayley <96212232+HayleyIsCool@users.noreply.github.com>
2023-01-11 11:17:23 -0800

    Added RobotMap.java

---
+++ src/main/java/org/team1515/botmitzvah/RobotMap.java
@@ -0,0 +1,14 @@
+package org.team1515.botmitzvah;
+
+public class RobotMap {
+
+    // Conversion factors
+    public final static double INCHES_TO_METERS = 0.0254;
+    public final static double FALCON_SENSOR_UNITS = 4096.0;
+
+    // Subsystems
+
+    // Limelight
+
+    // Drivetrain
+}
