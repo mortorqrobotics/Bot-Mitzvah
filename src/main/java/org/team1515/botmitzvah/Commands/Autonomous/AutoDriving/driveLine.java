4bb0bddcaac0543fc5d1c053a85a7471c2f66bb8
Daniel Salkin <daniel@walden.vc>
2023-10-11 18:09:18 -0700

    theoretical driveLine implementation

---
+++ src/main/java/org/team1515/botmitzvah/Commands/Autonomous/AutoDriving/driveLine.java
@@ -0,0 +1,37 @@
+package org.team1515.botmitzvah.Commands.Autonomous.AutoDriving;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+import org.team1515.botmitzvah.Subsystems.Drivetrain;
+
+public class driveLine extends CommandBase {
+    private final Drivetrain drivetrain;
+    private double i; //i vector component
+    private double j; //j vector component
+    private double ti; //initial time parameter
+    private double tf; //final time parameter
+    private double realTime; //actial system time
+    public ArmExtend(double i double j, double ti, double tf) {
+        this.arm = arm;
+        addRequirements(i);
+        addRequirements(j);
+        addRequirements(ti);
+        addRequirements(tf);
+        realTime = System.currentTimeMillis();
+    }
+
+    @Override
+    public void execute() {
+        drivetrain.drive(i,j,true,true)
+    }
+
+    @Override
+    public boolean isFinished() {
+        return System.currentTimeMillis()-realTime >= tf-ti
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        drivetrainSubsystem.drive(new Translation2d(0.0, 0.0), 0.0, false, false);
+    }
+}
